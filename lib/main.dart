3e0630722421c4a588b45a2d7277c703dbd52bed
Marwa-sh9 <marwa333777@hotmail.com>
2026-04-03 18:03:00 +0300

    Initial commit: Pixel Adventure game with Flame & Tiled

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:pixel_adventure/pixel_adventure.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  PixelAdventure game =PixelAdventure();
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
+  runApp(GameWidget(game: kDebugMode? PixelAdventure():game),
+  
+  );
+}
